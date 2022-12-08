58ffa243240bbf747624f2608b4fc2691f76609a
Mustafa Mohamed <mustafa.mohamed@kcl.ac.uk>
2022-12-08 17:37:31 +0000

    Exercie 3.1 completed

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,24 @@
 """Forms of the project."""
+from django import forms
+from django.core.validators import RegexValidator
+from things.models import Thing
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        fields = ['name', 'description', 'quantity']
+        widgets = {'description': forms.Textarea}
+
+    def clean(self):
+        """Cleans the data and rejects invalid inputs"""
+        super().clean()
+
+    def save(self):
+        super().save()
+        thing = Thing.objects.create(
+            name = self.cleaned_data.get('name'),
+            description = self.cleaned_data.get('description'),
+            quantity = self.cleaned_data.get('quantity'),
+        )
+        return thing
+
